2d603e2cd1117b89bf39a7382ebbb15f0b90e431
pcomlib <114752637+pcomlib@users.noreply.github.com>
2026-08-27 10:28:07 -0400

    More voyager clean

--- proxy-widgets.js
+++ proxy-widgets.js
@@ -103,28 +103,8 @@ window.addEventListener('load', function(event) {
 		output.focus();
 		output.select();
 	});
-	
-	document.querySelector('#voy_converter button').addEventListener('click', function(evt) {
-		let url = document.getElementById('voy_input').value;
-		let output = document.getElementById('voy_output');
-		let voy_re1 = /bibId=([0-9]+)/;
 
-		let match = url.match(voy_re1);
-		
-		if(match != null) {
-			output.value = 'http://pcom.hosted.exlibrisgroup.com/vwebv/holdingsInfo?bibId=' + match[1];
-			output.focus();
-			output.select();
-		}
-		else {
-			output.value = 'bibID Not Found';
-			output.focus();
-			output.select();
-		}
-
-	});
-	
-		document.querySelector('#ebsco_converter button').addEventListener('click', function(evt) {
+	document.querySelector('#ebsco_converter button').addEventListener('click', function(evt) {
 		let url = document.getElementById('ebsco_input').value;
 		let output = document.getElementById('ebsco_output');
 		
